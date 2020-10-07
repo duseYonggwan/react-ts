f1f3a27c7ac67ace79217384ec7e030b478b48bd
yonggwan <choiyk1225@gmail.com>
2020-10-07 15:33:48 +0900

    fix env

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = {
     compress: true,
     port: process.env.port || 3000,
     open: false,
-    host: process.env.APP_ENV,
+    host: process.env.APP_HOST,
     hot: true,
   },
   plugins: [
